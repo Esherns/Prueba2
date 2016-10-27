f95e9693bd5efa650d6584b11e3f6b1ad405dfe0
diego92sigma6 <d.marquezp@alumnos.duoc.cl>
2016-10-27 12:33:05 -0300

    sqlexception

--- src/java/ctrl/CargarCancionesServlet.java
+++ src/java/ctrl/CargarCancionesServlet.java
@@ -38,8 +38,9 @@ public class CargarCancionesServlet extends HttpServlet {
      *
      * @param request servlet request
      * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     * @throws javax.servlet.ServletException
+     * @throws java.io.IOException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException{
